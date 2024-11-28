8d0996f394c78cbf51a155582d91b55cada86865
akirial <trebardav@gmail.com>
2024-11-27 20:54:46 -0600

    adding storage hopefully

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,24 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+    name: 'amplifyTeamDrive',
+    access: (allow) => ({
+      'profile-pictures/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete'])
+      ],
+      'picture-submissions/*': [
+        allow.authenticated.to(['read','write']),
+        allow.guest.to(['read', 'write'])
+      ],
+      'profile-videos/{entity_id}/*': [
+        allow.guest.to(['read']),
+        allow.entity('identity').to(['read', 'write', 'delete']),
+      ],
+      // General video submissions: Both authenticated users and guests can view and upload
+      'video-submissions/*': [
+        allow.authenticated.to(['read', 'write']),
+        allow.guest.to(['read', 'write']),
+      ],
+    })
+  });
