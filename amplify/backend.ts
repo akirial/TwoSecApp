181fb33a50a40f28dedd70a564e555180e800c8d
akirial <trebardav@gmail.com>
2024-11-28 23:59:17 -0600

    tryna fix dynmao and storage still

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-
+import { aws_dynamodb } from "aws-cdk-lib";
 const backend = defineBackend({
   auth,
   data,
@@ -16,6 +16,21 @@ backend.addOutput({
 });
 
 
+const externalDataSourcesStack = backend.createStack("MyExternalDataSources");
+
+
+const tables = [
+  { name: "Comment-ashkgnrhvnhfpmqy524hjvgpsy-NONE", dataSourceName: "ExternalCommentTableDataSource" },
+  { name: "Friend-rvkj7qs6ffdyhjaqdvrdws7bn4-NONE", dataSourceName: "ExternalFriendTableDataSource" },
+  { name: "User-rvkj7qs6ffdyhjaqdvrdws7bn4-NONE", dataSourceName: "ExternalUserTableDataSource" },
+  { name: "Video-ashkgnrhvnhfpmqy524hjvgpsy-NONE", dataSourceName: "ExternalVideoTableDataSource" }
+];
+
+// Dynamically create data sources for each table
+tables.forEach(({ name, dataSourceName }) => {
+  const table = aws_dynamodb.Table.fromTableName(externalDataSourcesStack, name, name);
+  backend.data.addDynamoDbDataSource(dataSourceName, table);
+});
 
 
 //https://amplify-twosecapp-eleva-s-amplifyteamdrivebucket28-fpzgdu4hit7l.s3.us-east-2.amazonaws.com/video-submissions/video-1732839036270.mp4
